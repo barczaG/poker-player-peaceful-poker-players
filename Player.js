eade97d2c9b3efceeea7f3298c8279c172688bb1
Gergo Barcza <gergo@barcza.hu>
2017-01-14 15:49:40 +0100

    Table finished (#6)

--- Player.js
+++ Player.js
@@ -1,29 +1,6 @@
-var Logger = require('le_node')
-var log = new Logger({
-  token: 'fda2a63e-3dd4-4b70-9b73-097d51eb8d6d'
-})
-
 const cards2Table = require('./cards-2-table')
-
-// function toNum (c) {
-//   switch (c) {
-//     case 'J':
-//       return 11
-//     case 'Q':
-//       return 12
-//     case 'K':
-//       return 13
-//     case 'A':
-//       return 14
-
-//     default:
-//       return parseInt(c)
-//   }
-// }
-
-// function cardsValue (cards) {
-//   return toNum(cards[0].rank) + toNum(cards[1].rank)
-// }
+const strategyTable = require('./strategy-table')
+const efCalculator = require('./effective-stack-calculator')
 
 function isFolded (gameState) {
   return gameState.pot === (gameState.small_blind * 3)
@@ -41,18 +18,18 @@ function isFolded (gameState) {
 //   return !gameState.community_cards.length
 // }
 
-// function posHelper (firstPos, mePos, playerCount) {
-//   if (firstPos > mePos) firstPos = firstPos - playerCount
-//   const pos = mePos - firstPos
-//   const after = playerCount - pos
-//   return {pos, after}
-// }
+function posHelper (firstPos, mePos, playerCount) {
+  if (firstPos > mePos) firstPos = firstPos - playerCount
+  const pos = mePos - firstPos
+  const after = playerCount - pos
+  return {pos, after}
+}
 
-// function getPosition (gameState, myPlayer) {
-//   const firstPlayerIndex = (gameState.dealer + 3)
-//   const myIndex = myPlayer.id
-//   return posHelper(firstPlayerIndex, myIndex, gameState.players.length)
-// }
+function getPosition (gameState, myPlayer) {
+  const firstPlayerIndex = (gameState.dealer + 3)
+  const myIndex = myPlayer.id
+  return posHelper(firstPlayerIndex, myIndex, gameState.players.length)
+}
 
 class Player {
   static get VERSION () {
@@ -64,28 +41,20 @@ class Player {
 
     try {
       const cards = myPlayer.hole_cards
-          // const cValue = cardsValue(cards)
 
       const percentage = cards2Table.getPercentage(cards)
-
       const folded = isFolded(gameState)
-      // const posAfter = getPosition(gameState, myPlayer).after
-      if (folded && percentage <= 30) {
-        bet(myPlayer.stack)
-      } else if (folded && percentage <= 60) {
-          bet(0)
-          //current_buy_in - players[in_action][bet] + minimum_raise
-      } else if (!folded && percentage <= 12) {
+      const strategyQuery = {effStack: efCalculator(gameState), playersBehind: getPosition(gameState, myPlayer).after, folded}
+      const strategyPercentage = strategyTable.query(strategyQuery)
+
+      if (percentage < strategyPercentage) {
         bet(myPlayer.stack)
       } else {
         bet(0)
       }
-
-      log.info(gameState)
-      log.info(`${JSON.stringify(myPlayer)} ${JSON.stringify(cards)} ${JSON.stringify(percentage)}`)
     } catch (e) {
-      log.error('Exception: ' + e.message)
-      bet(myPlayer.stack)
+      // global.log.error('Exception: ' + e.message)
+      bet(0)
     }
   }
 
