de1fe5cc9d47b6ea1b993192d880e22864849580
Gergo Barcza <gergo@barcza.hu>
2017-01-14 17:10:33 +0100

    Staging (#9)
    * Table finished
    
    * Commiting stuf

--- test.js
+++ test.js
@@ -8,6 +8,11 @@ const cards2Table = require('./cards-2-table')
 const effectiveStackCalculator = require('./effective-stack-calculator')
 const gameStateFixture = require('./game-state-fixture.json')
 
+const strategyTable = require('./strategy-table')
+const strategyTableFixture = require('./test/strategy-table-fixture.json')
+
+const posHelper = require('./pos-helper')
+
 // test.skip('correct card value',
 //   function (t) {
 //     t.equal(playerJs.toNum('J'), 11)
@@ -15,9 +20,9 @@ const gameStateFixture = require('./game-state-fixture.json')
 //   }
 // )
 
-test('correct effectiveStack',function (t) {
-	t.equal(effectiveStackCalculator.calculate(gameStateFixture), 50.5)
-	t.end()
+test('correct effectiveStack', function (t) {
+  t.equal(effectiveStackCalculator.calculate(gameStateFixture), 50.5)
+  t.end()
 })
 
 test('cards 2 table convert should work', (t) => {
@@ -45,6 +50,42 @@ test('cards 2 table percentage should work', (t) => {
 
   cards = [{rank: 'A', suit: 'spades'}, {rank: 'Q', suit: 'cubes'}]
   t.equal(cards2Table.getPercentage(cards), 10.65)
+
+  t.end()
+})
+
+test('strategy table', (t) => {
+  let query = {effStack: 50, playersBehind: 2, folded: true}
+  t.equal(strategyTable.query(query, strategyTableFixture), 13)
+
+  t.end()
+})
+
+test('poshelper poshelper', (t) => {
+  let pos = posHelper.posHelper(4, 2, 5)
+  t.eq(pos, {pos: 4, after: 1})
+
+  pos = posHelper.posHelper(2, 4, 5)
+  t.eq(pos, {pos: 3, after: 2})
+
+  t.end()
+})
+
+test('poshelper getPosition', (t) => {
+  let pos = posHelper.posHelper(4, 2, 5)
+  t.eq(pos, {pos: 4, after: 1})
+
+  pos = posHelper.posHelper(2, 4, 5)
+  t.eq(pos, {pos: 3, after: 2})
+
+  pos = posHelper.posHelper(2, 4, 4)
+  t.eq(pos, {pos: 3, after: 2})
+
+  pos = posHelper.posHelper(1, 2, 3)
+  t.eq(pos, {pos: 3, after: 2})
+
+  pos = posHelper.posHelper(2, 1, 3)
+  t.eq(pos, {pos: 3, after: 2})
 
   t.end()
 })
