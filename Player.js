6d056d630b19c0366836b032cb5cc5e628c56455
barczaG <gergo@barcza.hu>
2017-01-14 10:24:15 +0100

    Introduce folded

--- Player.js
+++ Player.js
@@ -23,6 +23,10 @@ function cardsValue (cards) {
   return toNum(cards[0].rank) + toNum(cards[1].rank)
 }
 
+function isFolded (gameState) {
+  return gameState.pot === (gameState.small_blind * 3)
+}
+
 class Player {
   static get VERSION () {
     return '0.3'
@@ -32,13 +36,19 @@ class Player {
     const myPlayer = gameState.players[gameState.in_action]
     const cards = myPlayer.hole_cards
     const cValue = cardsValue(cards)
-    log.info(gameState)
-    log.info(`${JSON.stringify(myPlayer)} ${JSON.stringify(cards)} ${JSON.stringify(cValue)}`)
-    if (cValue > 15) {
-      return bet(myPlayer.stack)
+
+    const folded = isFolded(gameState)
+
+    if (folded && cValue > 15) {
+      bet(myPlayer.stack)
+    } else if (!folded && cValue > 24) {
+      bet(myPlayer.stack)
     } else {
       bet(0)
     }
+
+    log.info(gameState)
+    log.info(`${JSON.stringify(myPlayer)} ${JSON.stringify(cards)} ${JSON.stringify(cValue)}`)
   }
 
   static showdown (gameState) {
